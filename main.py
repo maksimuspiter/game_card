a4012d431066c859d770c0eaf3c45e49cf755529
MAkswdrfgv <maksimuspiter@gmail.com>
2022-11-29 23:05:08 +0300

    added coin, change move

--- main.py
+++ main.py
@@ -3,13 +3,6 @@ from random import choice, randrange
 from time import sleep
 from math import ceil
 
-options_health = {1: randrange(1, 3),
-          2: randrange(1, 5),
-          3: randrange(1, 7),
-          4: randrange(1, 9),
-          5: randrange(1, 11),
-          6: randrange(1, 14)}
-
 r_star = f'\033[31m{"*"}\033[0m'
 g_star = f'\033[32m{"*"}\033[0m'
 w_star = f'\033[37m{"*"}\033[0m'
@@ -34,12 +27,12 @@ class GameField:
     #         for j in range(self.dimension):
     #             print(self.field[i][j], end='')
     #         print()
-    def show_field_3(self):
-        for i in range(self.dimension):
-            for j in range(8):
-                print("".join(self.field[i][0].get_big_form()[j]),
-                      "".join(self.field[i][1].get_big_form()[j]),
-                      "".join(self.field[i][2].get_big_form()[j]))
+    def show_field_big(self):
+        for i in range(0, self.dimension):
+            for row in range(8):
+                for j in range(self.dimension):
+                    print("".join(self.field[i][j].get_big_form()[row]), end=' ')
+                print()
             print()
 
     def add_hero(self, hero):
@@ -50,7 +43,7 @@ class GameField:
                 if i == j == self.dimension // 2:
                     self.field[i][j] = hero
                 else:
-                    self.field[i][j] = choice([GoodCards(), BadCards()])
+                    self.field[i][j] = choice([GoodCards(), BadCards(), Coin()])
         pos = self.dimension // 2
         self.position_hero = (pos, pos)
 
@@ -103,6 +96,8 @@ class GameField:
                 hero_card.change_health(-extra)
                 if hero_card.health < 0:
                     flag = False
+            elif absorbed_card.CODE == "COIN":
+                hero_card.coins += absorbed_card.cost
 
             """
                 Герой не может сходить, если при этом ходе его здоровье упадет меньше 0
@@ -117,23 +112,32 @@ class GameField:
                     !!!!   the cards behind the vector of his movement move     !!!!
                     !!!!   in the same direction as the hero                    !!!!
                 """
-                new_card = choice([GoodCards(), BadCards(), EmptyCards()])
+                new_card = choice([GoodCards(), BadCards(), EmptyCards(), Coin()])
                 edge_map = (0, self.dimension-1)
 
                 # Если стоит в углу
                 if x_hero in edge_map and y_hero in edge_map:
 
                     self.field[x_hero][y_hero], self.field[x_hero + x_delta][y_hero + y_delta] = \
-                        new_card, self.field[x_hero][y_hero]
+                        EmptyCards(), self.field[x_hero][y_hero]
+
+                    try:
+                        self.field[x_hero][y_hero], self.field[x_hero - y_delta][y_hero - x_delta] = \
+                            self.field[x_hero - y_delta][y_hero - x_delta], new_card
+                    except IndexError:
+                        self.field[x_hero][y_hero], self.field[x_hero + y_delta][y_hero + x_delta] = \
+                            self.field[x_hero + y_delta][y_hero + x_delta], new_card
+
 
                 # Если стоит у края, только по одной координате касается границы
                 elif x_hero in edge_map or y_hero in edge_map:
 
                     # Если движется по ряду --> карты последуют за героем
-                    if (x_delta and x_hero - x_delta in range(self.dimension)
-                            or y_delta and y_hero - y_delta in range(self.dimension)):
+                    if (x_delta != 0 and x_hero - x_delta in range(self.dimension)
+                            or y_delta != 0 and y_hero - y_delta in range(self.dimension)):
 
                         counter = 0
+                        print('sdffsd')
                         while True:
 
                             counter += 1
@@ -151,39 +155,19 @@ class GameField:
                                 break
 
                     else:
-                        """
-                            Если движется вниз/вверх ↑ ↓ карта последует за героем (только одна).
-                            На место карты, которая последует за героем появится новая, случайная карта.
-                            
-                            Если герой ПОВЫШАЕТ координату по X за ним следует карта координата которой по Y МЕНЬШЕ.
-                            Если герой УМЕНЬШАЕТ координату по X за ним следует карта координата которой по Y БОЛЬШЕ.
-    
-                            Если герой ПОВЫШАЕТ координату по Y за ним следует карта координата которой по Y БОЛЬШЕ.
-                            Если герой УМЕНЬШАЕТ координату по Y за ним следует карта координата которой по Y МЕНЬШЕ.
-                        """
 
                         # Общее перемещение карты героя в нужном направлении и появление карты EmptyCards()
                         self.field[x_hero][y_hero], self.field[x_hero + x_delta][y_hero + y_delta] = \
                             EmptyCards(), self.field[x_hero][y_hero]
-                        # move_down
-                        # if x_delta == 1:
-                        #     pass
-                        #
-                        # # move_up
-                        # elif x_delta == -1:
-                        #     pass
-                        #
-                        # # move_right
-                        # elif y_delta == 1:
-                        #     pass
-                        #
-                        # # move_left
-                        # elif y_delta == -1:
-                        #     pass
-                        x_delta_new, y_delta_new = -y_delta, -x_delta
 
-                        self.field[x_hero][y_hero], self.field[x_hero + x_delta_new][y_hero + y_delta_new] = \
-                            self.field[x_hero + x_delta_new][y_hero + y_delta_new], new_card
+
+                        if x_delta:
+                            self.field[x_hero][y_hero], self.field[x_hero - y_delta][y_hero - x_delta] = \
+                                self.field[x_hero - y_delta][y_hero - x_delta], new_card
+                        else:
+                            self.field[x_hero][y_hero], self.field[x_hero + y_delta][y_hero + x_delta] = \
+                                self.field[x_hero + y_delta][y_hero + x_delta], new_card
+
 
                 # не касается края поля
                 else:
@@ -238,6 +222,8 @@ class Hero(Card):
     def __init__(self, health):
         self.health = health
         self.max_health = 10
+        self.coins = 0
+        self.weapons = []
 
     def __str__(self):
         return self.IMG_CARD_TEST
@@ -254,16 +240,17 @@ class Hero(Card):
 
 
     def show_hero_card(self):
-        print(f"{'_' * 6}\n"
-              f"|{' ' * 4}|\n"
-              f"{'| '}{self.IMG_CARD_TEST}{' |'}\n"
-              f"|{' '}{self.health if self.health // 10 > 0 else ' ' + str(self.health)}{' '}|\n"
-              f"|{' ' * 4}|\n"
-              f"{'¯' * 6}\n"
-              f"Health: {self.health}")
+        print(f"{'_' * 14}\n\n"
+              f"{self.IMG_CARD_TEST}\n"
+              f"Health: {self.health}\n"
+              f"Coins: {self.coins}\n"
+              f"{('Weapons:', self.weapons) if self.weapons else ''}")
 
     def change_health(self, extra):
         self.health += extra
+        if self.health > self.max_health:
+            self.health = self.max_health
+
 
 
 class EmptyCards(Card):
@@ -345,6 +332,26 @@ class BadCards(Card):
 
 class Coin(GoodCards):
     CODE = 'COIN'
+    title = "COIN"
+    IMG_CARD_TEST = f'M'
+    cost = None
+
+    def __new__(cls, level_field=4):
+        instance = super().__new__(cls)
+        instance.health = randrange(1, 9)
+        instance.cost = randrange(5, 10)
+        return instance
+
+    def get_big_form(self):
+        return [[p_star * 14],
+                [p_star, ' ' * 12, p_star],
+                [p_star, ' ' * 12, p_star],
+                [p_star, ' ' * 12, p_star],
+                [p_star, ' ' * 12, p_star],
+                [p_star, ' ' * ((12 - len(self.title))//2), self.title, ' ' * ceil((12 - len(self.title))/2), g_star],
+                [p_star, ' ' * (12 - len(str(self.cost))-1), str(self.cost), " ", g_star],
+                [p_star * 14]]
+
 
 
 def play():
@@ -374,11 +381,13 @@ def play():
             f1.move(move_right())
         elif direction == "stop":
             return True
-    f1 = GameField(3)
+
+    os.system('cls||clear')
+    f1 = GameField(randrange(3, 6))
     f1.create_field()
     hero = Hero(10)
     f1.add_hero(hero)
-    f1.show_field_3()
+    f1.show_field_big()
     hero.show_hero_card()
 
     while True:
@@ -387,11 +396,11 @@ def play():
         os.system('cls||clear')
         if hero.get_health() <= 0:
             hero.health = 0
-            f1.show_field_3()
+            f1.show_field_big()
             print("You are lose!")
             break
 
-        f1.show_field_3()
+        f1.show_field_big()
         hero.show_hero_card()
 
 if __name__ == '__main__':
